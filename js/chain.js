ffa25b7507f9f42a9a1a24b0eea75e86bf5bdc21
gennadiy1970 <ggm_kharkiv@ukr.net>
2018-08-18 00:18:30 +0300

    add a page about a chain

---
+++ js/chain.js
@@ -0,0 +1,47 @@
+const wrp = document.querySelector(".wrp");
+const panel = document.querySelector(".panel");
+const salePanel = document.querySelector(".sale-panel");
+const btna = document.getElementById("id1");
+const btnb = document.getElementById("id2");
+const btnc = document.getElementById("id3");
+wrp.addEventListener("click", showGoods);
+
+function showGoods(evt) {
+  if (evt.target.tagName.toLowerCase() !== "button") {
+    return null;
+  }
+  panel.innerHTML = evt.target.dataset.letter.toUpperCase();
+}
+
+function a () {
+    return new Promise(function(res, rej){
+      btna.addEventListener('click', function(){
+        res('A');
+        rej(new Error);
+      })
+    });
+  }
+  function b () {
+    return  new Promise(function(res, rej){
+      btnb.addEventListener('click', function(){
+        res('B');
+        rej(new Error);
+      })
+    });
+  }
+  function c () {
+    return new Promise(function(res, rej){
+      btnc.addEventListener('click', function(){
+        res('C');
+        rej(new Error);
+      })
+    });
+  }
+  
+  b().then(a)
+    .then(a)
+    .then(c)
+    .then(d => salePanel.textContent = 'Вы набрали BAAC')
+    .catch(err =>  salePanel.textContent = 'Error')
+  
+  
